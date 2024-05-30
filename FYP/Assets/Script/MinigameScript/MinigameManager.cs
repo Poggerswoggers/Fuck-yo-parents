aadd436056af6dbf4f60abd0fb2ac5d50bfd2348
Poggerswoggers <jasonsohhuihong@gmail.com>
2024-05-30 13:13:17 +0800

    Added 2D depth and some instructions

--- FYP/Assets/Script/MinigameScript/MinigameManager.cs
+++ FYP/Assets/Script/MinigameScript/MinigameManager.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 using System;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class MinigameManager : MonoBehaviour
 {
     public GameObject gameOverPanel;
+    [TextArea(2,5)]
+    [SerializeField] string instructionText;
+    [SerializeField] TextMeshProUGUI instructionTextField;
     private void OnEnable()
     {
         HeadTilt.OnGameOver += GameOver;
@@ -15,6 +19,18 @@ public class MinigameManager : MonoBehaviour
     private void OnDisable()
     {
         HeadTilt.OnGameOver -= GameOver;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(LoadInstructionCo());
+    }
+
+    IEnumerator LoadInstructionCo()
+    {
+        instructionTextField.text = instructionText;
+        yield return new WaitForSeconds(1f);
+        instructionTextField.text = "";
     }
 
     void GameOver()
