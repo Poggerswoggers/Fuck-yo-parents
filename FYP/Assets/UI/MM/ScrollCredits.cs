5b1157bd715e8ce9bfb2a36ab87e11f753602536
jaxlua <jaxxlua@gmail.com>
2024-08-06 22:36:36 +0800

    Credits Updates

--- FYP/Assets/UI/MM/ScrollCredits.cs
+++ FYP/Assets/UI/MM/ScrollCredits.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class ScrollCredits : MonoBehaviour {
-    public float scrollSpeed = 30f; 
-    public float stopPositionY = 100f; 
+public class ScrollCredits : MonoBehaviour, IDragHandler, IEndDragHandler {
+    public float scrollSpeed = 30f;
+    public float stopPositionY = 100f;
 
     private RectTransform rectTransform;
     private float initialPositionY;
+    private bool isDragging = false;
 
     void Start() {
         rectTransform = GetComponent<RectTransform>();
@@ -13,16 +15,26 @@ public class ScrollCredits : MonoBehaviour {
     }
 
     void Update() {
+        if (!isDragging) {
+            float newY = rectTransform.anchoredPosition.y + scrollSpeed * Time.deltaTime;
 
-        float newY = rectTransform.anchoredPosition.y + scrollSpeed * Time.deltaTime;
+            if (newY >= stopPositionY) {
+                newY = stopPositionY;
+                enabled = false;
+            }
 
-        // Stop scrolling if the position reaches or exceeds the stop position
-        if (newY >= stopPositionY) {
-            newY = stopPositionY;
-            enabled = false; 
+            rectTransform.anchoredPosition = new Vector2(rectTransform.anchoredPosition.x, newY);
         }
+    }
 
-        // Apply the new position
-        rectTransform.anchoredPosition = new Vector2(rectTransform.anchoredPosition.x, newY);
+    public void OnDrag(PointerEventData eventData) {
+        isDragging = true;
+        Vector2 newPos = rectTransform.anchoredPosition + new Vector2(0, eventData.delta.y);
+        newPos.y = Mathf.Clamp(newPos.y, initialPositionY, stopPositionY);
+        rectTransform.anchoredPosition = newPos;
+    }
+
+    public void OnEndDrag(PointerEventData eventData) {
+        isDragging = false;
     }
 }
