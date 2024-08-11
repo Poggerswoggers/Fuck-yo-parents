7040d52e4631bcd87c75b0ce8d4c933b7333f5d5
j0w33n <131142695+j0w33n@users.noreply.github.com>
2024-08-11 16:50:30 +0800

    scroll menu

---
+++ FYP/Assets/Script/SwipeController.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SwipeController : MonoBehaviour, IEndDragHandler
+{
+    [SerializeField] int maxPage;
+    int currentPage;
+    Vector3 targetPos;
+    [SerializeField] Vector3 pageStep;
+    [SerializeField] RectTransform pagesRect;
+
+    [SerializeField] float tweenTime;
+    [SerializeField] LeanTweenType tweenType;
+    float dragThreshold;
+
+    [SerializeField] Image[] barImage;
+    [SerializeField] Sprite barClosed, barOpen;
+
+    [SerializeField] Button prevBtn, nextBtn;
+
+    private void Awake()
+    {
+        currentPage = 1;
+        targetPos = pagesRect.localPosition;
+        dragThreshold = Screen.width / 15;
+        UpdateBar();
+        UpdateArrowBtn();
+    }
+
+    public void Next()
+    {
+        if (currentPage < maxPage)
+        {
+            currentPage++;
+            targetPos += pageStep;
+            MovePage();
+        }
+    }
+
+    public void Previous()
+    {
+        if (currentPage > 1)
+        {
+            currentPage--;
+            targetPos -= pageStep;
+            MovePage();
+        }
+    }
+
+    void MovePage()
+    {
+        pagesRect.LeanMoveLocal(targetPos, tweenTime).setEase(tweenType);
+        UpdateBar();
+        UpdateArrowBtn();
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (Mathf.Abs(eventData.position.x - eventData.pressPosition.x) > dragThreshold)
+        {
+            if (eventData.position.x > eventData.pressPosition.x) Previous();
+            else Next();
+          
+        }
+        else
+        {
+            MovePage();
+        }
+    }
+
+    void UpdateBar()
+    {
+        foreach (var item in barImage)
+        {
+            item.sprite = barClosed;
+        }
+        barImage[currentPage - 1].sprite = barOpen;
+    }
+
+    void UpdateArrowBtn()
+    {
+        nextBtn.interactable = true;
+        prevBtn.interactable = true;
+        if (currentPage == 1) prevBtn.interactable = false; // disables 'prev' arrow on first pg
+        else if (currentPage == maxPage) nextBtn.interactable = false; // disables 'next' arrow on last pg
+    }
+}
